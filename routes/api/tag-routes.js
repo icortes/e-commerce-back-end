a147869a91de88fae976255f3ee14185cd08c6e6
isa <5445946+icortes@users.noreply.github.com>
2021-10-28 00:02:51 -0700

    '/api/tags/:id/' PUT route complete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    tagData
+      ? res.status(200).json(tagData)
+      : res.status(404).json({ message: 'No tag found by this id!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
