ec007ac341b4517c0e9b1f382fde0748283eea9f
isa <5445946+icortes@users.noreply.github.com>
2021-10-27 18:37:23 -0700

    fixed api/categories/ to include products associated to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({include: Product});
     const categories = categoryData.map((category) =>
       category.get({ plain: true })
     );
