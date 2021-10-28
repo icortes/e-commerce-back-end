8739f6b0344879615b1bbd1f8b452198e4b81327
isa <5445946+icortes@users.noreply.github.com>
2021-10-27 19:27:11 -0700

    '/api/categories/:id' PUT route completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,16 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id }
+    });
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
