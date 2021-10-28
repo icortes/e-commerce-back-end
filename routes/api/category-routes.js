ed26b6cbdb03e23a37aef231fcaec4f7625d7f7d
isa <5445946+icortes@users.noreply.github.com>
2021-10-27 18:49:05 -0700

    '/api/categories/:id' endpoint completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll({include: Product});
+    const categoryData = await Category.findAll({ include: Product });
     const categories = categoryData.map((category) =>
       category.get({ plain: true })
     );
@@ -18,9 +18,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product
+    });
+    categoryData
+      ? res.status(200).json(categoryData)
+      : res.status(404).json({ message: 'No category found with this id!' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
